74f7a6b32f74a5bc616a7a7ebda891f365410e63
joseduard <joseduardo.jep@gmail.com>
2021-11-05 14:25:48 +0100

    CRUD creation ok

--- app/controllers/labelController.js
+++ app/controllers/labelController.js
@@ -1,62 +1,91 @@
-const { List, Card, Label } = require("../models/index");
+const { findByPk } = require("../models/card");
+const { Card, Label } = require("../models/index");
 
 const labelController = {
-    // async car on utlise sequelize
-    getAllLabels: async (req, res) => {
-        try {
-            const cards = await Card.findAll({
-                include: {
-                   all: true,
-                   nested: true, 
-                },
+  // async car on utlise sequelize
+  getAllLabels: async (req, res) => {
+    try {
+      const label = await Label.findAll();
+      res.json(label);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
 
-                order: [
-                    ["position", "ASC"],
-                ]
-            })
+  getOneLabel: async (req, res) => {
+    try {
+    } catch (error) {}
+  },
 
-            res.json(cards);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json(error.toString());
+  createLabel: async (req, res) => {
+    try {
+      const { name, color } = req.body;
+
+      let bodyErrors = [];
+      if (!name) {
+        bodyErrors.push("name can not be empty");
+      }
+      if (!color) {
+        bodyErrors.push("color can not be empty");
+      }
+
+      if (bodyErrors.length) {
+        res.status(400).json(bodyErrors);
+      } else {
+        let newLabel = Label.build({ name, color });
+        await newLabel.save();
+        res.json(newLabel);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  },
+
+  updateLabel: async (req, res) => {
+    try {
+      const labelId = req.params.id;
+      const { name, color } = req.body;
+      // On recupere un labl en base
+      let label = await findByPk(labelId);
+      if (!label) {
+        res.status(404).json("Can not find label with id " + labelId);
+      } else {
+        // la modification
+        if (name) {
+          label.name = name;
         }
-    },
 
-    getOneLabel: async (req,res) => {
-        try {
-            
-        } catch (error) {
-            
+        if (color) {
+          label.color = color;
         }
-    },
+        // on save
+        await label.save();
+        // response
+        res.json(label)
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  },
 
-    createLabel: async (req,res) => {
-        try {
-            
-        } catch (error) {
-            
-        }
-    },
-
-    updateLabel: async (req,res) => {
-        try {
-            
-        } catch (error) {
-            
-        }
-    },
-
-    deleteLabel: async (req,res) => {
-        try {
-            
-        } catch (error) {
-            
-        }
-    },
-
-
-
-
+  deleteLabel: async (req, res) => {
+    try {
+      const labelId = req.params.id;
+      let label = await Label.findByPk(labelId);
+      if (!label) {
+        res.status(404).json("Can not find label with id " + labelId);
+      } else {
+        await label.destroy();
+        res.json("Deleted label with id " + labelId);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  },
 };
 
 module.exports = labelController;
